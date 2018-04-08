e3f3e16249b3a4f1657e91547257393d7465983c
yinjimmy <jimmy.yin5@gmail.com>
2018-04-08 22:23:56 +0800

    fix lua

--- lua/frameworks/runtime-src/Classes/lua_module_register.h
+++ lua/frameworks/runtime-src/Classes/lua_module_register.h
@@ -11,8 +11,7 @@
 #include "3d/lua_cocos2dx_3d_manual.h"
 #include "audioengine/lua_cocos2dx_audioengine_manual.h"
 #ifdef SDKBOX_ENABLED
-#include "Plugin$(SDKBOX_PLUGIN_NAME)Lua.hpp"
-#include "Plugin$(SDKBOX_PLUGIN_NAME)LuaHelper.h"
+#include "PluginTapcoreLua.hpp"
 #endif
 #include "physics3d/lua_cocos2dx_physics3d_manual.h"
 #include "navmesh/lua_cocos2dx_navmesh_manual.h"
@@ -30,8 +29,7 @@ static int lua_module_register(lua_State* L)
     register_cocos3d_module(L);
     register_audioengine_module(L);
 #ifdef SDKBOX_ENABLED
-    register_all_Plugin$(SDKBOX_PLUGIN_NAME)Lua(L);
-    register_all_Plugin$(SDKBOX_PLUGIN_NAME)Lua_helper(L);
+    register_all_PluginTapcoreLua(L);
 #endif
 #if CC_USE_3D_PHYSICS && CC_ENABLE_BULLET_INTEGRATION
     register_physics3d_module(L);
